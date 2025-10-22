4bd4c20df90f68acbed91eacdff2595d5e9f9d39
Erik Stankevich | MikroPlan GmbH <ees@mikroplan.com>
2025-10-22 08:11:55 +0200

    hover

--- Red2.cs
+++ Red2.cs
@@ -8,6 +8,10 @@ public partial class Red2 : Area2D
 		// Correct signal connection
 		Connect("area_entered", new Callable(this, nameof(OnAreaEntered)));
 		InputPickable = true;
+
+
+		Connect("mouse_entered", new Callable(this, nameof(OnMouseEntered)));
+		Connect("mouse_exited", new Callable(this, nameof(OnMouseExited)));
 	}
 
 	
@@ -40,5 +44,14 @@ public partial class Red2 : Area2D
 			GD.Print("Player touched red square!");
 			area.Set("global_position", new Vector2(0, 0));
 		}
+	}
+
+	private void OnMouseEntered()
+	{
+		GD.Print("Hovering over Red2!");
+	}
+	private void OnMouseExited()
+	{
+		GD.Print("Mouse left Red2.");
 	}
 }
