16150ff0a8b2fe9536b7e2e8a9d1af6a81c89958
Erik Stankevich | MikroPlan GmbH <ees@mikroplan.com>
2025-10-27 08:09:41 +0100

    scale

--- blue.cs
+++ blue.cs
@@ -8,12 +8,19 @@ public partial class blue : Area2D
 	private float velocity = 300f;
 	private Vector2 axis = new Vector2(0, -1);
 
+	private Vector2 originalScale;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		originalScale = Scale;
+
 		 Connect("area_entered", new Callable(this, nameof(OnAreaEntered)));
 		// InputPickable = true;
+		//
+		Connect("mouse_entered", new Callable(this, nameof(OnMouseEntered)));
+		Connect("mouse_exited", new Callable(this, nameof(OnMouseExited)));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,5 +40,16 @@ public partial class blue : Area2D
 		{
 			area.Set("global_position", new Vector2(0, 0));
 		}
+	}
+
+	private void OnMouseEntered()
+	{
+		GD.Print("Hovered");
+		Scale = originalScale * 1.1f;
+	}
+	private void OnMouseExited()
+	{
+		GD.Print("Exited");
+		Scale = originalScale;
 	}
 }
